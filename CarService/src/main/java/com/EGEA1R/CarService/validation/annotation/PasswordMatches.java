048716320e19b584254517ef26b6b69cb7053379
Dániel Becze <f34l3ssqq990@gmail.com>
2020-12-29 11:43:40 +0100

    email template

--- CarService/src/main/java/com/EGEA1R/CarService/validation/annotation/PasswordMatches.java
+++ CarService/src/main/java/com/EGEA1R/CarService/validation/annotation/PasswordMatches.java
@@ -1,5 +1,8 @@
 package com.EGEA1R.CarService.validation.annotation;
 
+import com.EGEA1R.CarService.validation.PasswordMatchesChangePasswordValidator;
+import com.EGEA1R.CarService.validation.PasswordMatchesPasswordresetValidator;
+import com.EGEA1R.CarService.validation.PasswordMatchesSignupValidator;
 import com.EGEA1R.CarService.validation.PasswordMatchesValidator;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
@@ -15,9 +18,9 @@ import javax.validation.Payload;
 
 @Target({ TYPE, ANNOTATION_TYPE })
 @Retention(RUNTIME)
-@Constraint(validatedBy = PasswordMatchesValidator.class)
+@Constraint(validatedBy = {PasswordMatchesSignupValidator.class, PasswordMatchesPasswordresetValidator.class, PasswordMatchesChangePasswordValidator.class})
 @Documented
-public @interface PasswordMatchesSignup {
+public @interface PasswordMatches {
 
     String message() default "Passwords don't match";
 
