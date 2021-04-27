87879fb6c41b01ed6bd1436507ddbcc1b07a687f
Dániel Becze <f34l3ssqq990@gmail.com>
2021-04-27 21:11:03 +0200

    bug fixes

--- CarService/src/main/java/com/EGEA1R/CarService/persistance/repository/interfaces/TokenBlockRepository.java
+++ CarService/src/main/java/com/EGEA1R/CarService/persistance/repository/interfaces/TokenBlockRepository.java
@@ -1,11 +1,12 @@
-package com.EGEA1R.CarService.persistance.repository;
+package com.EGEA1R.CarService.persistance.repository.interfaces;
 
 import com.EGEA1R.CarService.persistance.entity.TokenBlock;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TokenBlockRepository extends CrudRepository<TokenBlock, Long> {
 
-    Optional<TokenBlock> findByUserId(Long userId);
+    List<TokenBlock> findByUserId(Long userId);
 }
